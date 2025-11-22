1b8521b0568e86528ab0b9bfed2e1d6886089b75
Annmariya <annmariyawilson31@gmail.com>
2025-11-22 09:34:17 +0530

    first commit

--- src/server.ts
+++ src/server.ts
@@ -5,13 +5,26 @@ import cors from "cors";
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
 
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://wellcheck-frontend.vercel.app"
+];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 app.get("/hello", (req: Request, res: Response) => {
   console.log("called hello api");
   res.json({ message: "Hello API working!" });
 });
+
 app.use("/api", routes);
 
 app.listen(PORT, () => {
